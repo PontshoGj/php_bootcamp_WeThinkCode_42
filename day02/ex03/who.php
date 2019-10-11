38ff68cfdc9e6cf4d2ebaba3137b58bacee04e8f
Pontsho MOGWERE <pmogwere@c5r5s6.wethinkcode.co.za>
2019-10-11 11:14:29 +0200

    day02 ex03 done

--- day02/ex03/who.php
+++ day02/ex03/who.php
@@ -1,5 +1,38 @@
 #!/usr/bin/php
 <?php
+    function ft_split($s, $c)
+    {
+        $arrsp = explode($c, $s);
+        $i = 0;
+        $al = array();
+        while ($i < count($arrsp))
+        {
+            if (($arrsp[$i] != ""))
+                $al[] = $arrsp[$i];
+            $i++;
+        }
+        return ($al);
+    }
+    function gettty($s)
+    {
+        $i =  posix_ttyname ($s);
+        $d = ft_split($i, "/");
+        return $d[1];
+    }
     $s = shell_exec('w');
-    
+    $a = ft_split($s, "\n");
+    $i = 2;
+    $q = array();
+    while ($i < count($a))
+        $q[] = ft_split($a[$i++], " ");
+    $i = 0;
+    date_default_timezone_set("Africa/Johannesburg");
+    while ($i < count($q))
+    {
+        if (strcmp($q[$i][1], "console") == 0)
+            echo $q[$i][0]. " ".$q[$i][1]."  ".date("M d")." ".$q[$i][3]."\n";
+        else
+            echo $q[$i][0]. " ".gettty($q[$i][1])."  ".date("M d")." ".$q[$i][3]."\n";
+        $i++;
+    }
 ?>
--- day02/ex04/photos.php
+++ day02/ex04/photos.php
@@ -1,15 +1,55 @@
 #!/usr/bin/php
 <?php
+    function ft_split($s, $c)
+    {
+        $arrsp = explode($c, $s);
+        $i = 0;
+        $al = array();
+        while ($i < count($arrsp))
+        {
+            if (($arrsp[$i] != ""))
+                $al[] = $arrsp[$i];
+            $i++;
+        }
+        return ($al);
+    }
     $dir = file_get_contents($argv[1]);
     $q = preg_split("/\n/", $dir);
     $file = substr(strstr($argv[1], "https://"), 8, strlen($argv[1]));
+    $harry = array();
+    while ($i < count($q))
+    {
+        if(strstr($q[$i],"<img"))
+            $harry[] = ft_split(strstr($q[$i],"<img"), " "); 
+        $i++;
+    }
+    $hname = array();
+    for($j = 0; $j < count($harry); $j++)
+    {
+        for($x = 0; $x < count($harry[$j]); $x++)
+        {
+            if (strstr($harry[$j][$x], "src"))
+                $hname[] = substr(trim($harry[$j][$x]), 5, (strlen(trim($harry[$j][$x])) - 6));
+        }
+    }
+    $sname = array();
+    for($j = 0; $j < count($hname); $j++)
+        $sname[] = ft_split($hname[$j],"/");
+    $cwd = array();
+    for($j = 0; $j < count($sname); $j++)
+        $cwd[] = getcwd()."/".$file."/".$sname[$j][count($sname[$j]) - 1];
+    //print_r($cwd);
     $g = "mkdir ".$file;
     $s = shell_exec($g);
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $argv[1]);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    $dat = curl_exec($ch);
+    for($j = 0; $j < count($sname); $j++)
+    {
+        curl_setopt($ch, CURLOPT_URL, $hname[$j]);
+        $fp = fopen($cwd[$j], 'wb');
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $dat = curl_exec($ch);
+        fclose($fp);
+    }
     curl_close($ch);
-    file_put_contents("home_big.jpg", $dat);
 ?>
